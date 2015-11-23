58acb1ff972ea27f49a820363265f7e833878c47
s.tokunaga <otokuna46@gmail.com>
2015-11-24 00:19:34 +0900

    Tutorial for the Dropwizard (from official HP)

---
+++ src/main/java/jp/otokunaga/example/helloworld/HelloWorldResource.java
@@ -0,0 +1,36 @@
+package jp.otokunaga.example.helloworld;
+
+/**
+ * Created by tokunaga on 11/23/15.
+ */
+import jp.otokunaga.example.helloworld.Saying;
+import com.google.common.base.Optional;
+import com.codahale.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Path("/hello-world")
+@Produces(MediaType.APPLICATION_JSON)
+public class HelloWorldResource {
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+
+    public HelloWorldResource(String template, String defaultName) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = new AtomicLong();
+    }
+
+    @GET
+    @Timed
+    public Saying sayHello(@QueryParam("name") Optional<String> name) {
+        final String value = String.format(template, name.or(defaultName));
+        return new Saying(counter.incrementAndGet(), value);
+    }
+}
---
+++ src/main/java/jp/otokunaga/example/helloworld/Saying.java
@@ -0,0 +1,33 @@
+package jp.otokunaga.example.helloworld;
+
+/**
+ * Created by tokunaga on 11/23/15.
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+
+public class Saying {
+    private long id;
+
+    @Length(max = 3)
+    private String content;
+
+    public Saying() {
+        // Jackson deserialization
+    }
+
+    public Saying(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    @JsonProperty
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public String getContent() {
+        return content;
+    }
+}
